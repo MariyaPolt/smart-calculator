465604a391fb439927880fcf65b9dbdc43a66d19
Mariya <mariya.poltoratskaya@gmail.com>
2018-10-05 07:14:47 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,27 +1,79 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.arr = initialValue;
   }
 
   add(number) {
-    // your implementation
+    this.arr += ' + ' + number;
+    return this;
   }
-  
+
   subtract(number) {
-    // your implementation
+    this.arr += ' - ' + number;
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.arr += ' * ' + number;
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.arr += ' / ' + number;
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.arr += ' ^ ' + number;
+    return this;
+  }
+
+  calculate() {
+    this.arr = this.arr.split(' ');
+      for (let i = this.arr.length - 1; i >= 0 ; i--) {
+        if (this.arr[i] == '^') {
+          this.arr[i - 1] = Math.pow(this.arr[i - 1], this.arr[i + 1]);
+          this.arr.splice(i, 2);
+        }
+      }
+
+      for (let i = this.arr.length - 1; i >= 0 ; i--) {
+        if (this.arr[i] == '*') {
+          this.arr[i - 1] = this.arr[i - 1] * this.arr[i + 1];
+          this.arr.splice(i, 2);
+        }
+      }
+
+      for (let i = this.arr.length - 1; i >= 0 ; i--) {
+        if (this.arr[i] == '/') {
+          this.arr[i - 1] = this.arr[i - 1] / this.arr[i + 1];
+          this.arr.splice(i, 2);
+        }
+      }
+
+      for (let i = 0; i < this.arr.length; i++) {
+        if (this.arr[i] == '-') {
+          this.arr[i - 1] = this.arr[i - 1] - this.arr[i + 1];
+          this.arr.splice(i, 2);
+          i--;
+        }
+      }
+
+      for (let i = 0; i < this.arr.length; i++) {
+        if (this.arr[i] == '+') {
+          this.arr[i - 1] = +this.arr[i - 1] + +this.arr[i + 1];
+          this.arr.splice(i, 2);
+          i--;
+        }
+      }
+
+    return this.arr[0];
+  }
+
+  valueOf() {
+    return this.calculate();
   }
 }
+
 
 module.exports = SmartCalculator;
